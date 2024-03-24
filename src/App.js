e3edd8a19b649569796e45e32248029ca1a11249
Матвей Петров <matveygemot@gmail.com>
2024-03-24 16:32:20 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import "./style.css";
 import { Button } from "./Button/index";
 import { useTelegram } from "./hooks/useTelegram";
 
+const tg = window.Telegram.WebApp;
+
 export const App = () => {
-  const { user, tg, onClose } = useTelegram();
+  const { user, onClose } = useTelegram();
 
   React.useEffect(() => {
     tg.ready();
