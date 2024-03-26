2ab14287f6168c82f9bba913c8b611e10965e3c9
Матвей Петров <matveygemot@gmail.com>
2024-03-26 20:36:54 +0700

    initial

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./item.css";
-import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { plusItem, minusItem } from "../../redux/slices/cartSlice";
 
