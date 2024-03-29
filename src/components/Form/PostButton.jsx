49a252410eae8f11b20c407faf5aa775c725e3c4
Матвей Петров <matveygemot@gmail.com>
2024-03-29 21:02:15 +0700

    initial

--- src/components/Form/PostButton.jsx
+++ src/components/Form/PostButton.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "./postbutton.css";
-import { Link } from "react-router-dom";
 
 export const PostButton = (props) => {
   return (
-    <Link onClick={props.onSendData} className="postbutton">
+    <div onClick={props.onSendData} className="postbutton">
       {props.text}
-    </Link>
+    </div>
   );
 };
