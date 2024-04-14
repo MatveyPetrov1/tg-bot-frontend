c264e1a52d85f5a9cd65d2872e55b07275a13d80
Матвей Петров <matveygemot@gmail.com>
2024-04-14 15:59:00 +0700

    initial

--- src/components/Home/ListItem/Listitem.jsx
+++ src/components/Home/ListItem/Listitem.jsx
@@ -4,6 +4,7 @@ import { plusItem, minusItem } from "../../../redux/slices/cartSlice";
 import { Link } from "react-router-dom";
 import "./listitem.css";
 import { Sirop } from "./Sirop";
+import "../../Cart/CartItem/cartitem.css";
 
 export const ListItem = ({
   title,
@@ -118,6 +119,16 @@ export const ListItem = ({
     };
     dispatch(plusItem(product));
   };
+
+  // React.useEffect(() => {
+  //   if (
+  //     siropValue.sirop1.length > 0 ||
+  //     siropValue.sirop2.length ||
+  //     siropValue.sirop3.length
+  //   ) {
+  //     onClickPlus();
+  //   }
+  // }, [siropValue]);
 
   // Minus item from cart
 
@@ -344,6 +355,33 @@ export const ListItem = ({
               </div>
             </>
           )}
+
+          <div className="value__block">
+            {sizeIndex === 0 && siropValue.sirop1.length > 0 ? (
+              <>
+                <h1>Сиропы:</h1>
+                {siropValue.sirop1.map((val) => (
+                  <h3>{val}</h3>
+                ))}
+              </>
+            ) : sizeIndex === 1 && siropValue.sirop2.length > 0 ? (
+              <>
+                <h1>Сиропы:</h1>
+                {siropValue.sirop2.map((val) => (
+                  <h3>{val}</h3>
+                ))}
+              </>
+            ) : sizeIndex === 2 && siropValue.sirop3.length > 0 ? (
+              <>
+                <h1>Сиропы:</h1>
+                {siropValue.sirop3.map((val) => (
+                  <h3>{val}</h3>
+                ))}
+              </>
+            ) : (
+              ""
+            )}
+          </div>
         </div>
 
         <div className="bottom">
