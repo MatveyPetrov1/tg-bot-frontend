c8fde62215c2ef40d552cc47840ff38da0471b5c
Матвей Петров <matveygemot@gmail.com>
2024-05-09 19:54:09 +0700

    tg button

--- src/components/Form/Form/Form.jsx
+++ src/components/Form/Form/Form.jsx
@@ -51,7 +51,7 @@ export const Form = () => {
   }, [form.name, form.number]);
 
   const onSendData = React.useCallback(async () => {
-    const { username } = window.Telegram.WebApp.initDataUnsafe.user;
+    const { userName } = window.Telegram.WebApp.initDataUnsafe.user;
 
     const product = {
       name: form.name,
@@ -59,7 +59,7 @@ export const Form = () => {
       street: form.street,
       time: form.time,
       comment: form.comment,
-      user: username,
+      userName,
       items,
     };
 
