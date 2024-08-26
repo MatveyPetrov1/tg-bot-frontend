6ad00189a005e62c9caccec1f8150faafa39072a
Матвей Петров <matveygemot@gmail.com>
2024-08-27 03:28:59 +0700

    payment

--- src/components/Form/Form/Form.jsx
+++ src/components/Form/Form/Form.jsx
@@ -61,6 +61,7 @@ export const Form = () => {
       comment: form.comment,
       userName: username,
       items,
+      userData: window.Telegram.WebApp.initDataUnsafe.user,
     };
 
     const checkData = (data) => {
